3fac3e431d0cf4dc34d03c5a9e8ead6ac6e6c380
Matt Fleming <122564640+chartpro-com@users.noreply.github.com>
2023-03-03 15:42:50 -0500

    first successful API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ var apiKey = '444d7ee70cf3d4f1caaf5b7385d3c7b8';
 var searchForm = document.querySelector('#search-form');
 var cityInput = document.querySelector('#city-input');
 var searchHistoryElement = document.querySelector('#search-history');
+var cityNameElement = document.querySelector('#city-name');
+var temperatureElement = document.querySelector('#temperature');
+
+
 
 var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || []
 
@@ -17,11 +21,37 @@ function renderSearchHistory() {
 searchForm.addEventListener('submit', (mainPage) => {
     mainPage.preventDefault();
     var city = cityInput.value.trim();
-  
+
     searchHistory.push(city);
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
     renderSearchHistory();
 });
+
+function renderCurrentWeather(city, weather) {
+    // remove miami
+    cityNameElement.textContent = `${city}`;
+    temperatureElement.textContent = `Temp: ${weather.main.temp} °F`;
+
+  }
+
+searchForm.addEventListener('submit', (searchAPI) => {
+    searchAPI.preventDefault();
+    var city = cityInput.value.trim();
+    console.log(city)
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
+.then(response => response.json())
+.then(data => {
+  renderCurrentWeather(city, data);
+    console.log(data)
+})
+
+
+cityInput.value = '';
+});
+
+
+
 
 
 
